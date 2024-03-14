11f65b68e0e9c8f98c4749cffb554095d13494e0
Nhat Lam Nguyen <135177207+nhlam3011@users.noreply.github.com>
2024-03-14 08:05:00 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 // Main.java
 public class Main {
     public static void main(String[] args) {
-        MP3 mp3 = new MP3("May phat MP3", "May phat nhac cam tay khong day", "MP3_011", 500f, 180);
+        MP3 mp3 = new MP3("May phat MP3", "May phat nhac cam tay khong day", "MP3_011", 200, 180);
         mp3.showInfo();
 
         System.out.println();
 
-        Book book = new Book("Thiet ke web co ban", "Day ngon ngu thiet ke web co ban cho nguoi moi", "BK-3001", 250.000f, "Nhat Lam", 456, "Lap Trinh");
+        Book book = new Book("Thiet ke web co ban", "Day ngon ngu thiet ke web co ban cho nguoi moi", "BK-3001", 15, "Nhat Lam", 456, "Lap Trinh");
         book.showInfo();
     }
 }
