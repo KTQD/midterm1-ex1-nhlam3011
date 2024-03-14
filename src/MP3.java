8ddb33b2b5d0489998ef5b25be74373934eb6096
Nhat Lam Nguyen <135177207+nhlam3011@users.noreply.github.com>
2024-03-14 08:03:53 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,17 @@
-public class MP3 {
+public class MP3 extends Item {
+    private int duration;
+
+    public MP3(String name, String description, String ID, float price, int duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Ten may MP3: " + name);
+        System.out.println("Mo ta: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Gia tien: " + price + " $");
+        System.out.println("Thoi luong: " + duration + " phut");
+    }
 }
