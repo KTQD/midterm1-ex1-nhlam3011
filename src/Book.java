919ad7b8444b8f9269dbfb51ccf9bcc5f0c10422
Nhat Lam Nguyen <135177207+nhlam3011@users.noreply.github.com>
2024-03-14 08:00:55 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,23 @@
-public class Book {
+public class Book extends Item {
+    private String author;
+    private int numberOfPages;
+    private String genre;
+
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Ten sach: " + name);
+        System.out.println("Mo ta: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Gia tien: " + price + " VND");
+        System.out.println("Tac gia: " + author);
+        System.out.println("So trang: " + numberOfPages);
+        System.out.println("The loai: " + genre);
+    }
 }
